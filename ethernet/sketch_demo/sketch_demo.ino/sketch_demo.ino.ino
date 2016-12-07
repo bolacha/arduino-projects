06c07d7ed46e69789a4cea80efe389ba22466ff7
Lucas Costa Oliveira <lcosta@br.ibm.com>
2016-12-07 18:42:24 -0200

    Added the First Project

---
+++ ethernet/sketch_demo/sketch_demo.ino/sketch_demo.ino.ino
@@ -0,0 +1,73 @@
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = { 0x0F, 0x5A, 0x08, 0xDD, 0xDD, 0x85 };
+
+char server[] = "192.168.0.172";    // name address for Google (using DNS)
+
+// My IP Address
+IPAddress ip(192, 168, 0, 177);
+
+EthernetClient client;
+
+unsigned long lastConnectionTime = 0;             // last time you connected to the server, in milliseconds
+const unsigned long postingInterval = 10L * 1000L; // delay between updates, in milliseconds
+// the "L" is needed to use long type numbers
+
+void setup() {
+  // start serial port:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // give the ethernet module time to boot up:
+  delay(1000);
+  // start the Ethernet connection using a fixed IP address and DNS server:
+  Ethernet.begin(mac, ip);
+  // print the Ethernet board/shield's IP address:
+  Serial.print("My IP address: ");
+  Serial.println(Ethernet.localIP());
+}
+
+void loop() {
+  // if there's incoming data from the net connection.
+  // send it out the serial port.  This is for debugging
+  // purposes only:
+  if (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  }
+
+  // if ten seconds have passed since your last connection,
+  // then connect again and send data:
+  if (millis() - lastConnectionTime > postingInterval) {
+    httpRequest();
+  }
+
+}
+
+// this method makes a HTTP connection to the server:
+void httpRequest() {
+  // close any connection before send a new request.
+  // This will free the socket on the WiFi shield
+  client.stop();
+
+  // if there's a successful connection:
+  if (client.connect(server, 3001)) {
+    Serial.println("connecting...");
+    // send the HTTP GET request:
+    client.println("GET /info HTTP/1.1");
+    client.println("Host: 192.168.0.172");
+    client.println("User-Agent: arduino-ethernet");
+    client.println("Connection: close");
+    client.println();
+
+    // note the time that the connection was made:
+    lastConnectionTime = millis();
+  } else {
+    // if you couldn't make a connection:
+    Serial.println("connection failed");
+  }
+}
